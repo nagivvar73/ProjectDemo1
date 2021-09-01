cc92867702961f4660bfba3051bd95422bc04317
veeravenkataap <veeravenkataap@outlook.com>
2021-09-01 20:34:41 +0530

    First123

---
+++ src/TestNG.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+public class TestNG
+{
+    @Test
+    public void function1()
+    {
+        System.out.println("Function1");
+    }
+
+    @Test
+    public void function2()
+    {
+        System.out.println("Function2");
+    }
+}
