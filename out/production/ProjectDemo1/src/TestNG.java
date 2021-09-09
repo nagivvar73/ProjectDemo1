c377c08cf5890ee3d326a8f7f876143b090c61e4
AutomationBadri007 <svbadrinath07@gmail.com>
2021-09-09 15:06:04 +0530

    Initial commit Badri

---
+++ out/production/ProjectDemo1/src/TestNG.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+public class TestNG
+{
+    @Test
+    public void function1()
+    {
+        System.out.println("Function1");
+    }
+
+    @Test
+    public void function2()
+    {
+        System.out.println("Function2");
+    }
+    @Test
+    public void function3()
+    {
+        System.out.println("Function3");
+    }
+}
